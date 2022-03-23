3dc3f6e2d97776813a70cbabe689c6fc6b2c8235
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2022-03-22 18:50:13 -0600

    updates proficiency view on about page

--- src/components/Proficiency.js
+++ src/components/Proficiency.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import {BiStar} from 'react-icons/bi'
 import '../styling/proficiency.css'
 
 const ProficiencySet = ({desc, value, list, typeFilter}) => {
   console.log(list)
 
+  const getStars = () => {
+    let stars = []
+    for(var i = 0; i < value; i++)
+      stars.push(<BiStar/>)
+    return stars
+  }
+
   return (
     <div className="proficiency-set">
-      <h1 className="proficiency-header">{desc}</h1>
+      <div className="proficiency-header">
+        <h1 className="proficiency-header">{desc}</h1>
+        {getStars()}
+      </div>
       <ol className="proficiency-list">
         {list.map((item, index) => <Proficiency key={index} title={item.title}
         type={item.type} typeFilter={typeFilter}/>)}
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ProficiencySet from '../components/Proficiency'
 import {BiStar} from 'react-icons/bi'
 import proficiencies from '../data/proficiencies'
+import '../styling/about.css'
 
 const About = () => {
   console.log(proficiencies.filter((x) => x.value === 3))
@@ -9,12 +10,16 @@ const About = () => {
   return (
     <div className="about">
       <p className="about-header">
-        This is a section all about me and the things I can do, and some that I can kinda do, but really very well.
+        This is a section all about me and the things I can do, and some that I can kinda do, but not really very well.
       </p>
       <h1 className="about-title">Proficiencies</h1>
       <div className="about-prof-box">
-        <ProficiencySet desc='confident' typeFilter='none'
-          list={proficiencies.filter((x) => x.value === 3)}/>
+        {
+          [1, 2, 3].map((num)=>
+            <ProficiencySet desc='confident' typeFilter='none' value={num}
+              list={proficiencies.filter((x) => x.value === num)}/>
+          )
+        }
       </div>
     </div>
   )
